6e0472c3c7d330762ad67fa58110fc9d563c1918
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-09-22 04:16:18 +0530

    Workmanager workrequest has been enqueued  and running successfully

--- app/src/main/java/com/theleafapps/pro/workmanager/MainActivity.java
+++ app/src/main/java/com/theleafapps/pro/workmanager/MainActivity.java
@@ -1,8 +1,15 @@
 package com.theleafapps.pro.workmanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Data data = new Data.Builder()
+                .putInt("number", 15)
+                .build();
+
+        // Constraints for the background task to be done
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.METERED)
+                .setRequiresCharging(true)
+                .build();
+
+        OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(SampleWorker.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                // setInitialDelay method delays the task scheduling by the time specified in the
+                // parameters 1st - Amount 2nd - unit of time
+                // below example delays the task scheduling with 4 minutes
+                .setInitialDelay(10, TimeUnit.SECONDS)
+                .addTag("download")
+                .build();
+
+        WorkManager.getInstance(this).enqueue(request);
     }
 }
